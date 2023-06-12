e5f8b0ddd842a0d9feee0313ddabcd98a27b8a1f
Abhishek Ingle <a.ingle@rnt.ai>
2023-06-12 19:38:50 +0530

    Hotel Service

---
+++ HotelServiceApp/src/main/java/com/micro/hotelservice/HotelServiceAppApplication.java
@@ -0,0 +1,13 @@
+package com.micro.hotelservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelServiceAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelServiceAppApplication.class, args);
+	}
+
+}
